f5e33287ab7c1e0460f997a16525b60ddc20be2f
lschirripa <lschirripa@frba.utn.edu.ar>
2021-09-24 00:43:41 -0300

    columna updated

--- afd.c
+++ afd.c
@@ -21,11 +21,31 @@ int Columna(int c)
     switch (c)
     {
     case '+':
-        return 2;
+        return 0;
     case '-':
-        return 2;
-    default: // es digito o letra
+        return 0;
+    case '1':
         return 1;
+    case '2':
+        return 1;
+    case '3':
+        return 1;
+    case '4':
+        return 1;
+    case '5':
+        return 1;
+    case '6':
+        return 1;
+    case '7':
+        return 1;
+    case '8':
+        return 1;
+    case '9':
+        return 1;
+    case '0':
+        return 1;
+    default: // si es letra
+        return 2;
     }
 }
 
