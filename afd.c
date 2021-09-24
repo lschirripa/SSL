20d5c315375767a6f32959cb900e5aaf57a9eb1c
lschirripa <lschirripa@frba.utn.edu.ar>
2021-09-23 23:26:07 -0300

    new fx columna

--- afd.c
+++ afd.c
@@ -6,7 +6,7 @@ int main()
 {
     char s1[50] = "";
     printf("ingrese una cadena \n");
-    scanf(% s, s1);
+    scanf("%s", s1);
 
     if (EsPalabra(s1))
         printf("La cadena s1 pertenece al lenguaje");
@@ -14,6 +14,39 @@ int main()
         printf("La cadena s1 no pertenece al lenguaje");
 
     return 0;
+}
+
+int Columna(int c)
+{
+    switch (c)
+    {
+    case '+':
+        return 2;
+    case '-':
+        return 2;
+    case '1':
+        return 2;
+    case '2':
+        return 2;
+    case '3':
+        return 2;
+    case '4':
+        return 2;
+    case '5':
+        return 2;
+    case '6':
+        return 2;
+    case '7':
+        return 2;
+    case '8':
+        return 2;
+    case '9':
+        return 2;
+    case '0':
+        return 2;
+    case '0x':
+        return 2;
+    }
 }
 
 int EsPalabra(const char *cadena)
@@ -29,7 +62,9 @@ int EsPalabra(const char *cadena)
             {7, 8, 8, 8}};
     int e;          // estado actual
     unsigned int i; //recorre la cadena
-    for (e = 0, i = 0; cadena[i] != '\0' && e != 3; i++)
+    for (e = 0, i = 0; cadena[i] != '\0' && e != 8; i++)
+        e = tt[e][Columna(cadena[i])];
+    return e == 2;
 }
 
 // int inicio = 1;
